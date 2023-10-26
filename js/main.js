6ebcda5788f8a91162638deaae118054ebdf3205
BartoPons <ponsbarto@gmail.com>
2023-10-26 22:21:49 +0200

    he intentado diferentes formas durante dos horas y he conseguido ubicar una imagen pero al intentar diferentes opciones nohe lo he conseguido finalmente.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,8 @@
+document.getElementById('pantalla').innerHTML = 'inicializando...';
 
-function cargarImagenConSonido(imagenSrc) {
-    var imagen = document.getElementById('imagen');
-    var sonido = document.getElementById('sonido');
 
-    if (imagenSrc === 'imagen1.jpg') {
-        imagen.src = imagenSrc;
-        imagen.style.display = 'block';
-        sonido.src = 'clic1.mp3';
-    } else if (imagenSrc === 'imagen2.jpg') {
-        imagen.src = imagenSrc;
-        imagen.style.display = 'block';
-        sonido.src = 'clic2.mp3';
-    } else if (imagenSrc === 'imagen3.jpg') {
-        imagen.src = imagenSrc;
-        imagen.style.display = 'block';
-        sonido.src = 'clic3.mp3';
-    } else {
-        console.log('Imagen no encontrada');
-    }
-
-    sonido.play();
+function clickBoton(color) {
+    console.log('pasa por aqui', color);
+    document.getElementById('pantalla').style.backgroundColor = color;
+    document.getElementById('pantalla').className = 'secundario';
 }
-
-
-document.getElementById('boton1').addEventListener('click', function() {
-    cargarImagenConSonido('imagen1.jpg');
-});
-document.getElementById('boton2').addEventListener('click', function() {
-    cargarImagenConSonido('imagen2.jpg');
-});
-document.getElementById('boton3').addEventListener('click', function() {
-    cargarImagenConSonido('imagen3.jpg');
-});
